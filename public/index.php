4cd43ecd5ace63ce0b34ae6a66f2dc7ac965350d
Mazarini <mazarini@mazarini.ovh>
2021-10-09 15:24:49 +0200

    Add tool for maintenance

--- public/index.php
+++ public/index.php
@@ -2,6 +2,20 @@
 
 use App\Kernel;
 
+if (file_exists('maintenance'))
+{
+   switch (true) {
+         case $_SERVER['REQUEST_URI'] == '/' || $_SERVER['REQUEST_URI'] == '/index.php' :
+            readfile('maintenance.html');
+            exit;
+         case  file_exists($img=ltrim($_SERVER['REQUEST_URI'],'/')):
+            break;
+         default :
+            header('Location: /',TRUE,307);
+            exit;
+   }
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
